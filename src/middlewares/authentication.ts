2c3e7c3df4666678635558c431bf29efca69992a
LiponRoy <liponbdbd@gmail.com>
2025-08-07 01:17:45 +0600

    remove unused import

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import config from '../config';
 import { catchAsyncError } from '../utils/catchAsyncErrors';
 import ApiError from '../errors/ApiError';
